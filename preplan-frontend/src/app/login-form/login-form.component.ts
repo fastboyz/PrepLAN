d1c55011e9685642207ec48f09f6e7a34ddb1961
May Pham <doan.may.pham@gmail.com>
2020-06-23 15:20:53 -0400

    Fix css login, added input and validation on registration form

--- preplan-frontend/src/app/login-form/login-form.component.ts
+++ preplan-frontend/src/app/login-form/login-form.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { $ } from 'protractor';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'login-form',
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-    document.body.className ='text-center';
+  isLogin: boolean;
+  constructor() { 
+    this.isLogin = true;
   }
 
+  ngOnInit(): void {
+    // document.body.className ='text-center';
+    this.isLogin = true;
+  }
+
+  ngOnDestroy(): void{
+
+  }
+
+  toggleLogin(){
+    this.isLogin = !this.isLogin
+  }
 }
---
+++ preplan-frontend/src/app/shared/models/user.ts
@@ -0,0 +1,14 @@
+export class User {
+    username: string;
+    password: string;
+    firstName: string;
+    lastName:string;
+    birthday: Date;
+    mobileNumber: string;
+    discord: string;
+    email: string;
+    pronoun: string;
+    allergy: string;
+    certification: string;
+    
+} 
