94f676f0ec7104163a3cceb9a083d7b99701a8d8
May Pham <doan.may.pham@gmail.com>
2020-07-23 21:05:16 -0400

    fix role guard

--- preplan-frontend/src/app/helpers/role.guard.ts
+++ preplan-frontend/src/app/helpers/role.guard.ts
@@ -1,30 +1,28 @@
 import { Injectable } from '@angular/core';
-import { 
-  Router,
-  CanActivate,
-  ActivatedRouteSnapshot,
-  RouterStateSnapshot
-} from '@angular/router';
+import { Router, CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+
 @Injectable()
 export class RoleGuard implements CanActivate {
-  constructor(public authService: AuthService, public router: Router) {}
+    constructor(public authService: AuthService, public router: Router) { }
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    const currentUser = this.authService.currentUserValue;
-    const expectedRole = route.data.expectedRole;
-    let currentRole: string;
-    this.authService.getRole().subscribe(role=>{
-        currentRole = role.name;
-    })
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const currentUser = this.authService.currentUserValue;
+        const expectedRole = route.data.expectedRole;
+        let currentRole: string;
+        this.authService.getRole().subscribe(role => {
+            currentRole = role.name;
+        })
 
-    if (currentUser || currentRole.toLowerCase() == expectedRole.toLowerCase()) {
-        // logged in so return true
-        return true;
+        if (currentUser && currentRole.toLowerCase() == expectedRole.toLowerCase()) {
+            if (currentRole.toLowerCase() == expectedRole.toLowerCase()) {
+                // logged in and role is expected so return true
+                return true;
+            }
+            this.router.navigate(['/'], { queryParams: { returnUrl: state.url } });
+            return false;
+        }
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
     }
-
-    // not logged in so redirect to login page with the return url
-    this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
-    return false;
-}
 }
