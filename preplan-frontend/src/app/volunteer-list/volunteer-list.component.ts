297ec5818fbce4e9c766463c7a6396f0fcb38301
May Pham <doan.may.pham@gmail.com>
2020-08-27 09:38:06 -0400

    Add filter to volunteerlist

--- preplan-frontend/src/app/volunteer-list/volunteer-list.component.ts
+++ preplan-frontend/src/app/volunteer-list/volunteer-list.component.ts
@@ -16,11 +16,13 @@ export class VolunteerListComponent implements OnInit {
   @Input() edition: Edition;
   viewOnly: boolean = true;
 
+  selectedFilter: any;
+  status = Status;
+  statusOptions = [];
+  allInscriptionCheckboxes: InscriptionCheckbox[] = [];
   inscriptionCheckboxes: InscriptionCheckbox[] = [];
   inscriptionList: InscriptionEvent[] = [];
   inscriptionEvent: InscriptionEvent;
-  json = '{"edition":{"startDate":"2020-07-17T15:14:00.000Z","endDate":"2020-07-20T03:14:00.000Z","name":"2020 hi ","isRegistering":false,"isActive":false,"location":"My backyard hi noon","event":{"title":"LanETS hi","description":"woohooo","id":"5f0d2326aba69d4518005435"},"id":"5f0d232baba69d4518005436"},"profile":{"tshirtSize":"M","allergy":"","certification":"","id":"5f3cbcfd8aeb2e555058629d","emergencyContact":{"firstName":"Mum","lastName":"Dad","phoneNumber":"111-222-3333","relationship":"Parent","id":"5f3cbcfd8aeb2e555058629c"},"canEdit":true,"user":{"firstName":"Testy","lastName":"Tester","birthday":"1999-02-21T00:00:00.000Z","phoneNumber":"123-123-1234","discord":"Rawr#1234","pronoun":"She/Her","account":{"email":"volunteer@rawr.test","username":"volunteer","password":"$2a$08$1FDiD.DUlej3qWv6jcw5S.DCzeZIRdCjwqfvDL7faJ63ecNTnhrxa","id":"5f3cbcfd8aeb2e555058629a","role":"volunteer"},"id":"5f3cbcfd8aeb2e555058629b"}},"availability":[{"startDate":"2020-07-17T04:00:00.000Z","endDate":"2020-07-17T12:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-17T12:00:00.000Z","endDate":"2020-07-17T16:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-17T16:00:00.000Z","endDate":"2020-07-17T20:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-17T20:00:00.000Z","endDate":"2020-07-18T00:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-18T00:00:00.000Z","endDate":"2020-07-18T03:59:59.999Z","state":"DESIRED"},{"startDate":"2020-07-18T04:00:00.000Z","endDate":"2020-07-18T12:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-18T12:00:00.000Z","endDate":"2020-07-18T16:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-18T16:00:00.000Z","endDate":"2020-07-18T20:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-18T20:00:00.000Z","endDate":"2020-07-19T00:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-19T00:00:00.000Z","endDate":"2020-07-19T03:59:59.999Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T04:00:00.000Z","endDate":"2020-07-19T12:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T12:00:00.000Z","endDate":"2020-07-19T16:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T16:00:00.000Z","endDate":"2020-07-19T20:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T20:00:00.000Z","endDate":"2020-07-20T00:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-20T00:00:00.000Z","endDate":"2020-07-20T03:59:59.999Z","state":"UNAVAILABLE"}],"preference":{"dailyMaxHours":"12h","departments":["Cashier","Patrol","Logistic","Construction","Tech Support","Vendor"]},"status":"PENDING"}'
-  json2 = '{"edition":{"startDate":"2020-07-17T15:14:00.000Z","endDate":"2020-07-20T03:14:00.000Z","name":"2020 hi ","isRegistering":false,"isActive":false,"location":"My backyard hi noon","event":{"title":"LanETS hi","description":"woohooo","id":"5f0d2326aba69d4518005435"},"id":"5f0d232baba69d4518005436"},"profile":{"tshirtSize":"M","allergy":"","certification":"","id":"5f3cbcfd8aeb2e555058629d","emergencyContact":{"firstName":"Mum","lastName":"Dad","phoneNumber":"111-222-3333","relationship":"Parent","id":"5f3cbcfd8aeb2e555058629c"},"canEdit":true,"user":{"firstName":"Testy 2","lastName":"Tester 2","birthday":"1993-02-21T00:00:00.000Z","phoneNumber":"123-123-1234","discord":"Rawr#1234","pronoun":"She/Her","account":{"email":"volunteer@rawr.test","username":"volunteer","password":"$2a$08$1FDiD.DUlej3qWv6jcw5S.DCzeZIRdCjwqfvDL7faJ63ecNTnhrxa","id":"5f3cbcfd8aeb2e555058629a","role":"volunteer"},"id":"5f3cbcfd8aeb2e555058629b"}},"availability":[{"startDate":"2020-07-17T04:00:00.000Z","endDate":"2020-07-17T12:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-17T12:00:00.000Z","endDate":"2020-07-17T16:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-17T16:00:00.000Z","endDate":"2020-07-17T20:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-17T20:00:00.000Z","endDate":"2020-07-18T00:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-18T00:00:00.000Z","endDate":"2020-07-18T03:59:59.999Z","state":"DESIRED"},{"startDate":"2020-07-18T04:00:00.000Z","endDate":"2020-07-18T12:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-18T12:00:00.000Z","endDate":"2020-07-18T16:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-18T16:00:00.000Z","endDate":"2020-07-18T20:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-18T20:00:00.000Z","endDate":"2020-07-19T00:00:00.000Z","state":"DESIRED"},{"startDate":"2020-07-19T00:00:00.000Z","endDate":"2020-07-19T03:59:59.999Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T04:00:00.000Z","endDate":"2020-07-19T12:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T12:00:00.000Z","endDate":"2020-07-19T16:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T16:00:00.000Z","endDate":"2020-07-19T20:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-19T20:00:00.000Z","endDate":"2020-07-20T00:00:00.000Z","state":"UNAVAILABLE"},{"startDate":"2020-07-20T00:00:00.000Z","endDate":"2020-07-20T03:59:59.999Z","state":"UNAVAILABLE"}],"preference":{"dailyMaxHours":"12h","departments":["Cashier","Construction","Vendor"]},"status":"PENDING"}'
 
   constructor(
     private route: ActivatedRoute,
@@ -31,22 +33,28 @@ export class VolunteerListComponent implements OnInit {
   ngOnInit(): void {
     let id = this.route.snapshot.paramMap.get('id');
     if (id != null || id != undefined) {
-      this.eventService.getAllInscriptionByEditionId(id).subscribe(data => {
-        data.forEach(element => {
-          let newEntry: InscriptionCheckbox = {
-            inscriptionEvent: element,
-            checked: false
-          }
-          this.inscriptionCheckboxes.push(newEntry);
-        })
+      this.eventService.getAllInscriptionByEditionId(id).subscribe(async (data) => {
+        await this.createData(data);
+        this.inscriptionCheckboxes = JSON.parse(JSON.stringify(this.allInscriptionCheckboxes));
       });
     }
+    this.statusOptions = Object.keys(this.status);
+    this.selectedFilter = "ALL";
   }
 
   loadModal(inscription: InscriptionEvent) {
     this.inscriptionEventFormComponent.loadInscriptionValues(inscription);
   }
 
+  async createData(data: InscriptionEvent[]) {
+    data.forEach(element => {
+      let newEntry: InscriptionCheckbox = {
+        inscriptionEvent: element,
+        checked: false
+      }
+      this.allInscriptionCheckboxes.push(newEntry);
+    })
+  }
   async approveSelected() {
     const approvedStatus = "APPROVED"
     let selected: InscriptionEvent[] = await this.getSelectedOptions();
@@ -55,7 +63,7 @@ export class VolunteerListComponent implements OnInit {
     this.eventService.updateAllInscriptionStatus(selected).subscribe();
     this.clearSelection();
   }
-  
+
   async disapproveSelected() {
     const disapprovedStatus = "DISAPPROVED";
     let selected: InscriptionEvent[] = await this.getSelectedOptions();
@@ -65,6 +73,37 @@ export class VolunteerListComponent implements OnInit {
     this.clearSelection();
   }
 
+  onFilterChange(evt: any) {
+    switch (this.selectedFilter) {
+      case Status.PENDING:
+        this.inscriptionCheckboxes = this.allInscriptionCheckboxes.filter(i =>
+          i.inscriptionEvent.status === Status.PENDING
+        );
+        break;
+
+      case Status.APPROVED:
+        this.inscriptionCheckboxes = this.allInscriptionCheckboxes.filter(i =>
+          i.inscriptionEvent.status === Status.APPROVED
+        );
+        break;
+
+      case Status.DISAPPROVED:
+        this.inscriptionCheckboxes = this.allInscriptionCheckboxes.filter(i =>
+          i.inscriptionEvent.status === Status.DISAPPROVED
+        );
+        break;
+
+      case Status.CANCELLED:
+        this.inscriptionCheckboxes = this.allInscriptionCheckboxes.filter(i =>
+          i.inscriptionEvent.status === Status.CANCELLED
+        );
+        break;
+
+      default:
+        this.inscriptionCheckboxes = this.allInscriptionCheckboxes;
+        break;
+    }
+  }
   getSelectedOptions() {
     return this.inscriptionCheckboxes
       .filter(opt => opt.checked)
@@ -89,4 +128,15 @@ export class VolunteerListComponent implements OnInit {
 class InscriptionCheckbox {
   inscriptionEvent: InscriptionEvent;
   checked: boolean;
+}
+
+class InscriptionStatus {
+  status: Status;
+}
+
+enum Status {
+  PENDING = "PENDING",
+  APPROVED = "APPROVED",
+  DISAPPROVED = "DISAPPROVED",
+  CANCELLED = "CANCELLED"
 }
